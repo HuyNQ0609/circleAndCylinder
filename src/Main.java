132c7729ff2317338a747671dea1b545eda04da9
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-01-12 16:27:16 +0700

    Circle and Cylinder

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10.0, "green");
+        System.out.println(circle);
+        Cylinder cylinder = new Cylinder(10.0, "blue", 8.0);
+        System.out.println(cylinder);
+    }
+}
